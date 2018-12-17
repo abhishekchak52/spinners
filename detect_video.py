b741cf7fa6ef6a8695284f58cdca36e70a383953
Abhishek Chakraborty <deadbeatfour@gmail.com>
2018-12-17 17:26:49 +0530

    Added tracking dots for the arms on the base frame view

--- detect_video.py
+++ detect_video.py
@@ -32,13 +32,22 @@ while(cap.isOpened()):
 
     # First we find the centres. We look at all contours from filtering the green marker. 
     # The 6 centres will have area ~350 so we select those contours specifically.
+    # Next we choose those pink patches with area greater than ~50
 
     _, green_ctrs, _ = cv2.findContours(centre_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    _, pink_ctrs, _ = cv2.findContours(arms_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
     centres = []
     for c in green_ctrs:
         M = cv2.moments(c)
         if M["m00"] > 300:
             centres.append(c)
+
+    arms = [] 
+    for a in pink_ctrs:
+        M = cv2.moments(a)
+        if M["m00"] > 50:
+            arms.append(a)
 
     # Now we draw and mark the centres of each green marker
     centre_coords = []
@@ -47,11 +56,25 @@ while(cap.isOpened()):
         # calculate x,y coordinate of center
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        cv2.circle(res, (cX, cY), 2, (0, 0, 255), -1)
-        cv2.circle(res, (cX, cY), 80, (0, 0, 255), 1)
+        cv2.circle(frame, (cX, cY), 2, (0, 0, 255), -1)
+        cv2.circle(frame, (cX, cY), 80, (0, 0, 255), 1)
         centre_coords.append([cX,cY])
 
-    cv2.drawContours(res,centres,-1,color=(255,0,0))  
+    # Let's find the centres of all the available arms
+
+    arms_coords = []
+    for a in arms:
+        M = cv2.moments(a)
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.circle(frame, (cX, cY), 2, (255, 255, 255), -1)
+        # cv2.circle(frame, (cX, cY), 80, (0, 0, 255), 1)
+        arms_coords.append([cX,cY])
+
+    # cv2.drawContours(res,centres,-1,color=(255,0,0))
+    # cv2.drawContours(frame,pink_ctrs,-1,color=(255,0,0))  
+
     window_size = 180
     for index, centre in enumerate(centre_coords):
         cen_x, cen_y = centre
@@ -59,13 +82,12 @@ while(cap.isOpened()):
         max_x = min(cen_x + window_size//2, 639)
         min_y = max(cen_y - window_size//2, 0)
         max_y = min(cen_y + window_size//2, 479)
-        # cv2.rectangle(res,(min_x,min_y),(max_x,max_y),(0,0,255))
 
         # Apparently for ROI the indices work in the reverse order [y,x]
-        roi = res[min_y:max_y,min_x:max_x]
+        roi = frame[min_y:max_y,min_x:max_x]
         cv2.imshow("spinner"+str(index), roi)
 
-    # cv2.imshow('Mask', mask)
+    # cv2.imshow('Mask', arms_mask)
     # cv2.imshow('Detected', res)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
